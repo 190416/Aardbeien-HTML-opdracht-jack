61633dbc40e4c74b083dbb18a9225ffbc3e13f03
Jack Tollenaar <95088427+190416@users.noreply.github.com>
2021-12-03 13:54:54 +0000

    Changes

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 
+
+
 //Random weetjes
 
 var weetjes = Array("Aardbeien zijn super lekker!", "Welkom bij aardbeien.nl", "Wist je dat aardbeien voor 89% uit water bestaan", "Wist je dat 9/10 van onze specialisten zeggen dat aardbeien lekker zijn", "Weetje: met aardbeien kun je super lekkere jam maken!", "Weetje: Aardbeien zijn rood, als ze dat niet zijn zijn ze niet meer goed", "De perfecte site voor aardbeien liefhebbers");
 
+var quizantwoord = Array("Een mens","Een aardbei","Een aardappel","Pindakaas","Verbrande patat","Verrote aardbei","De gouden aardbei","Teer");
+
+
 //RNG
 var rweetje = weetjes[Math.floor(Math.random()*weetjes.length)];
+
+var resultaat = "NULL";
 
 //Update pagina
 //Weetjes
@@ -16,10 +23,41 @@ var Vraag1 = -1;
 var Vraag2 = -1;
 var Vraag3 = -1;
 
-var Resultaten = Array("Een mens","Een aardbei","Een aardappel","Pindakaas","Verbrande patat","Verrote aardbei","De gouden aardbei");
 var Ant = 0;
 
 function Antwoord(vraag, antwoord) {
+
+//Button kleurverandering
+
+if (vraag == 1) {
+  document.getElementById("vraag1").className = "vraag";
+  document.getElementById("vraag2").className = "vraag";
+  document.getElementById("vraag3").className = "vraag";
+  document.getElementById("vraag4").className = "vraag";
+
+  document.getElementById("vraag" + antwoord).className = "vraagbeantwoord";
+}
+if (vraag == 2) {
+  document.getElementById("vraag5").className = "vraag";
+  document.getElementById("vraag6").className = "vraag";
+  document.getElementById("vraag7").className = "vraag";
+  document.getElementById("vraag8").className = "vraag";
+
+  document.getElementById("vraag" + (antwoord+4)).className = "vraagbeantwoord";
+}
+if (vraag == 3) {
+  document.getElementById("vraag9").className = "vraag";
+  document.getElementById("vraag10").className = "vraag";
+
+  document.getElementById("vraag" + (antwoord+8)).className = "vraagbeantwoord";
+}
+
+
+
+
+
+
+
 
 if (vraag == 1) {
   Vraag1 = antwoord;
@@ -34,6 +72,7 @@ if (vraag == 3) {
 //Antwoorden
 
 //Heel veel if statements :D
+
 if (Vraag1 == 1 || Vraag1 == 3) {
   if (Vraag2 == 1) {
     Ant = 4;
@@ -53,16 +92,22 @@ if (Vraag1 == 2) {
 if (Vraag1 == 4) {
   if (Vraag2 == 4) {
     Ant = 3;
+  }else if (Vraag2 == 1){
+    Ant = 8;
   }else {
-    Ant = 1
+    Ant = 1;
   }
 
 }else Ant = 0;
 
-var resultaat = Resultaten[math.floor(Ant)];
 
 }
 function Show() {
-  document.getElementById("antwoord").innerHTML = resultaat;
+  resultaat = "PATATJE";
+
+  resultaat = quizantwoord[Ant];
+
+  document.getElementById("ant").innerHTML = resultaat;
+
 }
 
